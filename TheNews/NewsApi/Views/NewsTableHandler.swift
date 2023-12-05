e8c4a0ba464151dd421e96fac92b08eadcdec223
babitarawat <A00280963@mycambrian.ca>
2023-12-05 14:29:40 -0500

    add News table handler and create table view on it

---
+++ TheNews/NewsApi/Views/NewsTableHandler.swift
@@ -0,0 +1,29 @@
+//
+//  NewsTableHandler.swift
+//  TheNews
+//
+//  Created by Babita Rawat on 2023-12-05.
+//
+
+import UIKit
+
+class NewsTableHandler: NSObject, UITableViewDataSource, UITableViewDelegate {
+
+    var articles: [Article] = []
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let item = articles[indexPath.row]
+        guard let url = item.url else { return }
+
+        UIApplication.shared.open(url)
+    }
+
+}
